05a03654701d12d20e69ad10c5ead82e6ee76405
hamja900 <luru@naver.com>
2024-02-07 10:47:14 +0900

    feat : 사용자지정 금액 입금 오류 수정

--- Assets/Scripts/Bank/Bank.cs
+++ Assets/Scripts/Bank/Bank.cs
@@ -6,6 +6,10 @@ using UnityEngine.UI;
 
 public class Bank : MonoBehaviour
 {
+    public GameObject player;
+    Chad chad;
+    
+
     public GameObject bankMainBtns;
     public GameObject depositWindow;
 
@@ -15,7 +19,15 @@ public class Bank : MonoBehaviour
 
     [Header("depositMenu")]
     public InputField inputfield;
-    
+   
+    [Header("PopUps")]
+    public GameObject noMoney;
+    public GameObject noInput;
+
+    private void Awake()
+    {
+        chad = player.GetComponent<Chad>();
+    }
     public void OnDepositBtn()
     {
         bankMainBtns.SetActive(false);
@@ -29,5 +41,71 @@ public class Bank : MonoBehaviour
         bankMainBtns.SetActive(true);
         AudioManager.Instance.ClickSound();
     }
-    
+
+    public void OnTenK()
+    {
+        if(chad.cash >= 10000)
+        {
+            chad.cash -= 10000;
+            chad.banlance += 10000;
+        }
+        else
+        {
+            noMoney.SetActive(true);
+        }
+    }
+    public void OnTirtyK()
+    {
+        if (chad.cash >= 30000)
+        {
+            chad.cash -= 30000;
+            chad.banlance += 30000;
+        }
+        else
+        {
+            noMoney.SetActive(true);
+        }
+    }
+    public void OnFiftyK()
+    {
+        if (chad.cash >= 50000)
+        {
+            chad.cash -= 50000;
+            chad.banlance += 50000;
+        }
+        else
+        {
+            noMoney.SetActive(true);
+        }
+    }
+
+    public void OnInputDeposit()
+    {
+        try
+        {
+            if (chad.cash >= int.Parse(inputfield.text))
+            {
+                chad.cash -= int.Parse(inputfield.text);
+                chad.banlance += int.Parse(inputfield.text);
+            }
+            else if ( chad.cash < int.Parse(inputfield.text))
+            {
+                noMoney.SetActive(true);
+            }
+        }
+        catch
+        {
+                noInput.SetActive(true);
+  
+        }
+        
+      
+    }
+
+    public void BankConfirmBtn()
+    {
+        noMoney.SetActive(false);
+        noInput.SetActive(false);
+        AudioManager.Instance.ClickSound();
+    }
 }
